975cfd6c6732339dbba5e8540b7a524e85184765
이윤서 <yyoo04@naver.com>
2025-08-07 10:42:28 +0900

    feat : Search Bar 컴포넌트 검색기능 구현

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import SearchBar from '../components/SearchBar';
 
 const Main = () => {
   return (
     <div>
       <h1>Welcome to the Main Page</h1>
+      <SearchBar />
     </div>
   );
 };
