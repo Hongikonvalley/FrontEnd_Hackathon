7a23bd0a3f0de559d67c4c7b12f46d37056c1da6
이윤서 <yyoo04@naver.com>
2025-08-10 12:37:10 +0900

    style : Stores Page UI 수정

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,20 +1,21 @@
 // src/components/Layout.jsx
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './Header'; // 새로 만든 Header import
 import Navbar from './Navbar'; // 수정된 Navbar import
 
 export default function Layout() {
+  const [showNavBar, setShowNavBar] = useState(true);
+
   return (
     <div>
       {/* {location.pathname !== '/main' && <Header />} */}
 
-      {/* pt-16과 pb-16을 모두 적용하여 위아래 공간을 확보합니다. */}
+      {showNavBar && <Navbar />}
       <div className="pt-8 pb-8">
-        <Outlet />
+        <Outlet context={{ setShowNavBar }} />
       </div>
-      <Navbar />
     </div>
   );
 }
--- src/pages/Stores.jsx
+++ src/pages/Stores.jsx
@@ -1,13 +1,35 @@
 // src/pages/Stores.jsx
 
 import React from 'react';
+import { useEffect } from 'react';
 import { stores } from '../data/mockStores'; // 전체 Mock Data 불러오기
 import StoreCard from '../components/StoreCard';
+import { useNavigate, useOutletContext } from 'react-router-dom';
 
 const Stores = () => {
+  const { setShowNavBar } = useOutletContext();
+  useEffect(() => {
+    setShowNavBar(false); // 페이지 들어올 때 숨김
+    return () => setShowNavBar(true); // 페이지 나갈 때 복구
+  }, [setShowNavBar]);
+
+  const nav = useNavigate();
+  const onClickBack = () => {
+    nav('/');
+  };
+
   return (
-    <div className="p-6 bg-gray-50">
-      <h1 className="text-3xl font-bold mb-6">전체 매장 목록</h1>
+    <div className="p-6 bg-white min-h-screen ">
+      <div className="flex flex-row items-center content-center justify-between mb-6">
+        <img
+          src="/Back.svg"
+          alt="Back"
+          className="w-[36px] h-[36px]"
+          onClick={() => onClickBack()}
+        />
+        <p className="text-[30px]">more;ing</p>
+        <img src="/Menu.png" alt="Menu" className="w-[40px] h-[40px]" />
+      </div>
       <div className="flex flex-col items-center gap-6 w-full max-w-4xl mx-auto">
         {/* stores 배열의 각 item을 store라는 이름으로 StoreCard에 전달 */}
         {stores.map((store) => (
