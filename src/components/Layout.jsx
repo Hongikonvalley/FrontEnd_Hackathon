68c66318628b64c62b248d0ddaf6aaab53c9cf35
이윤서 <yyoo04@naver.com>
2025-08-10 10:26:29 +0900

    style-fix : Nav Bar UI 수정

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,7 +8,7 @@ import Navbar from './Navbar'; // 수정된 Navbar import
 export default function Layout() {
   return (
     <div>
-      {location.pathname !== '/main' && <Header />}
+      {/* {location.pathname !== '/main' && <Header />} */}
 
       {/* pt-16과 pb-16을 모두 적용하여 위아래 공간을 확보합니다. */}
       <div className="pt-8 pb-8">
