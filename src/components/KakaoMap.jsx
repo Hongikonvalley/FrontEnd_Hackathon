0ffb756a3ed3799e0192db2c85303f38cf34c129
Leejungin <jung020623@g.hongik.ac.kr>
2025-08-14 00:33:20 +0900

    feat : kakaomap 컴포넌츠 추가(기능 구현 안함), storedetail 수정

---
+++ src/components/KakaoMap.jsx
@@ -0,0 +1,10 @@
+const KakaoMap = () => {
+  return (
+    <div>
+      <h1>Kakao Map Component</h1>
+      {/* Add your Kakao Map integration here */}
+    </div>
+  );
+};
+
+export default KakaoMap;
